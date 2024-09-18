706ac62dc49092fbb81ef5867cbc9e54fb75fe62
Shwetha Shetty K <104064727+ShwethaShettyK@users.noreply.github.com>
2024-09-18 11:33:16 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,21 +1,47 @@
 #include <assert.h>
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+const char* majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
+const char* minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+stringstream printColorMap() {
+    stringstream message("\n",ios_base::app | ios_base::out);
     int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
             std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
-    return i * j;
+    cout << message.str();
+    return message;
 }
 
 int main() {
-    int result = printColorMap();
-    assert(result == 25);
+    std::string outtext;
+    stringstream result (printColorMap().str());
+    string line;
+    vector<string> stringList;
+    // Loop until the end of the string
+    while (getline(result, line)) {
+        if (!line.empty()) {
+            stringList.push_back(line);
+        }
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            int code = (i * 5+ j);
+            string stringLine = stringList.at(code );
+            assert(stringLine.find(std::to_string(code+1)) != string::npos);
+            assert(stringLine.find(majorColor[i]) != string::npos);
+            assert(stringLine.find(minorColor[j]) != string::npos);
+
+        }
+    }
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
