ac058cd149ea43b02fefb4ab20b2d71803d5c750
Shwetha Shetty K <104064727+ShwethaShettyK@users.noreply.github.com>
2024-09-19 09:55:49 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -12,7 +12,7 @@ stringstream printColorMap() {
     int i = 0, j = 0;
     for (i = 0; i < 5; i++) {
         for (j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            message << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
     cout << message.str();
