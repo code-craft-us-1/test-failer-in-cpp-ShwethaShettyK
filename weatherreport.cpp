f5fc7201fb623d30df498a213dfdc0c9cf8ebbb5
Shwetha Shetty K <104064727+ShwethaShettyK@users.noreply.github.com>
2024-09-25 13:02:14 +0530

    Update weatherreport.cpp

--- weatherreport.cpp
+++ weatherreport.cpp
@@ -57,15 +57,23 @@ string Report(const IWeatherSensor& sensor) {
     // precipitation < 20 is a sunny day
     string report = "Sunny day";
 
-    if (sensor.TemperatureInC() > 25) {
-        if (precipitation >= 20 && precipitation < 60)
-            report = "Partly cloudy";
-        else if (sensor.WindSpeedKMPH() > 50)
-            report = "Alert, Stormy with heavy rain";
+     if (sensor.TemperatureInC() > 25) {
+         if (precipitation >= 20 && precipitation < 60) {
+             report = "Partly cloudy";
+         }
+         else if (precipitation > 60) {
+           if(sensor.WindSpeedKMPH() > 50) {
+                report = "Alert, Stormy with heavy rain";
+           }
+           else {
+                report = "rainy day";
+           }
+         }
     }
     return report;
 }
 
+ 
 // Test a rainy day
 
 void TestRainy() {
