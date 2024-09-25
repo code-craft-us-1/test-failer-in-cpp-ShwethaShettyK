1301e3147c4737ee24ad8efa1c76b761c2938c1e
Shwetha Shetty K <104064727+ShwethaShettyK@users.noreply.github.com>
2024-09-25 12:30:34 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,26 +1,55 @@
 #include <assert.h>
 #include <iostream>
 
-char size(int cms) {
-    char sizeName = '\0';
-    if (cms < 38) {
-        sizeName = 'S';
-    } else if (cms > 38 && cms < 42) {
-        sizeName = 'M';
-    } else if (cms > 42) {
-        sizeName = 'L';
+enum class Size
+{
+    XS,
+    S,
+    M,
+    L,
+    XL,
+    Invalid
+};
+
+Size size(int cms) {
+    Size sizeName = Size::Invalid;
+
+    switch(cms)
+    {
+        case 36:
+            sizeName = Size::XS;
+            break;
+        case 38:
+            sizeName =  Size::S;
+            break;
+        case 40:
+            sizeName = Size::M;
+            break;
+        case 42:
+            sizeName = Size::L;
+            break;
+        case 44:
+            sizeName = Size::XL;
+            break;
     }
     return sizeName;
 }
 
-int main() {
-    assert(size(38) == 'S');
-    assert(size(40) == 'M');
-    assert(size(42) == 'L');
-    assert(size(37) == '\0');
-    assert(size(39) == '\0');
-    assert(size(41) == '\0');
-    assert(size(43) == '\0');
+void testSize()
+{
+    assert(size(36) == Size::XS);
+    assert(size(38) == Size::S);
+    assert(size(40) == Size::M);
+    assert(size(42) == Size::L);
+    assert(size(44) == Size::XL);
+    assert(size(37) == Size::Invalid);
+    assert(size(39) == Size::Invalid);
+    assert(size(41) == Size::Invalid);
+    assert(size(43) == Size::Invalid);
     std::cout << "All is well (maybe!)\n";
+}
+
+int main() {
+    testSize();
     return 0;
 }
